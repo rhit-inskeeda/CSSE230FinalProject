a6872bfa32ef09a654930559665b5a03b78b457c
rhit-gawronja <gawronja@rose-hulman.edu>
2022-02-01 14:48:19 -0500

    Added todos and base jframe

--- src/RoseMapper.java
+++ src/RoseMapper.java
@@ -1,10 +1,17 @@
+import javax.swing.*;
 public class RoseMapper{
+    public void createMap(){
+        Jframe map = new JFrame();
+    }
     class Map{
         HashMap<String,T> nodeMap;
         Map(){
-            nodeMap=new HashMap();
+            this.nodeMap=new HashMap();
         }
     }
+    //TODO: Add image to the jframe of Rose-hulman map
+    //TODO: Determine if we are using A* of Djikstras algo
+    //TODO: Create buttons:
     class LocationNode{
         ArrayList<LocationNode> connections;
         T location;
